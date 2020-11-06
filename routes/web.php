8564173187560d93b08d77283150ac4e00bd8e10
abdoal <abdrahimalhyane5@gmail.com>
2020-11-06 20:19:37 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Mail\NewUserWelcomeMail;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/email', function () {
+    return new NewUserWelcomeMail();
+});
+
+Auth::routes();
+
+Route::post('follow/{user}', 'FollowsController@store');
+
+Route::get('/', 'PostsController@index');
+Route::get('/p/create', 'PostsController@create');
+Route::get('/p/{post}', 'PostsController@show');
+Route::post('/p', 'PostsController@store');
+
+Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.index');
+Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
+Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
